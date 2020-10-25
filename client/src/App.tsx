cf87c99333783baeecb5e8b25f0cb2fd0e987d5f
jonaskay <jonaskay@iki.fi>
2020-10-25 16:48:09 +0200

    Working hello world app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`http://${process.env.REACT_APP_API_BASE_URL}/api/v1/hello`)
+      .then((res) => res.json())
+      .then(
+        ({ message }) => {
+          setIsLoading(false);
+          setMessage(message);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{error === null ? message : (error as any).message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
